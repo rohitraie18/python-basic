99144738d9a62d5a7779cb350598d56c96b3d645
rohitraie18 <rohitraee07@gmail.com>
2024-06-11 12:38:48 +0545

    Update first.py

--- first.py
+++ first.py
@@ -6,3 +6,176 @@ print("KKK")
 a=2
 b=2
 print(a+b)
+
+
+print("Hello world")
+
+a=2
+b=3
+print(a+b)
+
+name="Ram"
+age=18
+location="lalitpur"
+print(name)
+print(age)
+print(location)
+
+
+
+
+name="Ram"
+print("My brother name is     :",    name)
+
+location="Lalitpur"
+print("He live in   ",location)
+
+
+
+Age=99
+print("He is",Age, "years old")
+
+
+name="ram"
+age=18
+location="KTM"
+print("My bro name is",name)
+print("He is ", age, "years old")
+
+
+#Data type
+name="Ram"
+age=18
+price=101.99
+a=True
+b=None
+print(type(name))  #str
+print(type(age))  #int
+print(type(price))  #float
+print(type(a))  #bool
+print(type(b))  #none type
+
+ 
+
+#types of operators
+#arithmetic operators
+a=50
+b=40
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b) #remainder
+print(a**b)  #a^b
+
+#relation/ comparison operators
+a=20
+b=10
+print(a==b) #false
+print(a!=b)  #true
+print(a>b)  #true
+print(a<b)  #false
+print(a>=b)  #true
+print(a<=b)  # false
+
+#assignment operators
+#long
+num=20
+num=10+num
+print("num :",num)  #output : 30
+#short
+num=20
+num +=10
+print("num :",num)  #output : 30
+
+
+num=20
+num-=10
+print("num :",num)  #output : 10
+
+num=20
+num*=10
+print("num :",num)  #output : 200
+
+num=20
+num/=10
+print("num :",num)  #output : 2
+
+num=20
+num%=10
+print("num :",num)  #output : 0
+
+num=2
+num**=3
+print("num :",num)  #output : 8
+
+num=20
+num=10
+print("num :",num)  #output : 10
+
+
+print("hello world")
+
+#types of operators
+#arithmetic operators
+a=50
+b=40
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b) #remainder
+print(a**b)  #a^b
+
+#relation/ comparison operators
+a=20
+b=10
+print(a==b) #false
+print(a!=b)  #true
+print(a>b)  #true
+print(a<b)  #false
+print(a>=b)  #true
+print(a<=b)  # false
+
+#assignment operators
+#long
+num=20
+num=10+num
+print("num :",num)  #output : 30
+#short
+num=20
+num +=10
+print("num :",num)  #output : 30
+
+
+num=20
+num-=10
+print("num :",num)  #output : 10
+
+num=20
+num*=10
+print("num :",num)  #output : 200
+
+num=20
+num/=10
+print("num :",num)  #output : 2
+
+num=20
+num%=10
+print("num :",num)  #output : 0
+
+num=2
+num**=3
+print("num :",num)  #output : 8
+
+num=20
+num=10
+print("num :",num)  #output : 10
+
+a=2
+b=2
+print(a+b)
+
+
+
+print("solukhumbu")
