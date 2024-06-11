8c49274776e55726bb3ef4b1057b374b38627fd0
rohitraie18 <rohitraee07@gmail.com>
2024-06-11 18:02:36 +0545

    g

--- questions.py
+++ questions.py
@@ -1,1 +1,23 @@
 print("Hello questions")
+#1 WAP to input two numbers and print their sum.
+first=int(input("Enter 1st number :"))
+second=float(input("Enter 2nd number :"))
+sum=first+second
+print("Sum=", sum)   #print(first+second)
+
+#2 WAp to input side of a square and print its area.
+L=float(input("Enter length of square:"))
+area=L**2
+print("area of square is :", area)
+
+#3 Wap to input 2 floating numbers and print their average.
+firstnum=float(input("Enter 1st number :"))
+secondnum=float(input("Enter 2nd number :"))
+a=firstnum+secondnum    #average=(firstnum + secondnum)/2
+average=a//2
+print("Total average=", average)
+
+#4 Wap to input 2 int numbers a and b. Print True if a is greater or equal to b. If not print False.
+a=int(input("Enter 1st num:"))
+b=int(input("Enter 2nd num:"))
+print(a>=b )
