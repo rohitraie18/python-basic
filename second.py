c77dff96f4e67c8400aef0cf3fa3e838498c9dbb
rohitraie18 <rohitraee07@gmail.com>
2024-06-12 11:59:43 +0545

    Update second.py

--- second.py
+++ second.py
@@ -1,1 +1,36 @@
 print("second lecture")
+
+#string
+str1='this is a string'
+str2="this is a string"
+str3="""this is a string"""
+print(str1)
+print(str2)
+print(str3)
+
+str1="I am Rohit. \t I am from Solu."  #\t-tab
+print(str1)
+
+#concatenation
+a="hello"
+b="world"
+print(a+b)
+
+#length of str
+str1="hello"
+len1=len(str1)    #print(len(str1))
+print(len1)
+
+str2="world"
+len2=len(str2)
+print(len2)
+
+final_str = str1 + " " + str2
+print(len(final_str))
+
+#Indexing  index-position
+str = "kanku school"
+chr = str[6]       #print(str[6])
+print(chr)
+
+#slicing
