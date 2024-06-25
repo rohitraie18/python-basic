4a4179ab5e69084bb4f579a93b5758f5ba9948e6
RoshanKhalinG <rohitraee07@gmail.com>
2024-06-25 17:16:13 +0545

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
 print("Hello practice")
 #WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
-input("Enter 1st movie name")
+first=input("Enter first movie name :")
+secon=input("Enter second movie name :")
