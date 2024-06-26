110c6777ca4f2d04131d220b8c537368ba362e48
RoshanKhalinG <rohitraee07@gmail.com>
2024-06-26 16:54:25 +0545

    h

--- practice.py
+++ practice.py
@@ -1,4 +1,11 @@
-print("Hello practice")
-#WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
-first=input("Enter first movie name :")
-secon=input("Enter second movie name :")
+#WAP to check if a list contains a palindrome of elements. (Hint: use copy( ) method)
+list1=[1,2,1,1]
+
+copy_list1=list1.copy()
+copy_list1.reverse()
+
+if(copy_list1==list1):
+  print("palindrome")
+
+else:
+  print("Not palindrom")
--- questions.py
+++ questions.py
@@ -90,3 +90,19 @@ else:
     print("This number is not multiple of 7")
 
 #12 WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+"""
+movies=[]
+mov=input("Enter first movie name :")
+mov1=input("Enter second movie name :")
+mov2=input("Enter third movie name :")
+movies.append(mov)
+movies.append(mov1)
+movies.append(mov2)
+print(movies)
+"""
+
+movies=[]
+movies.append(input("Enter 1st movie :"))
+movies.append(input("Enter 2nd movie :"))
+movies.append(input("Enter 3rd movie :"))
+print(movies)
