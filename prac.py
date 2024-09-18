dcc32012922a12df4cc987e363a56ef2c5714923
KHAL1NG HORAA <rohitraee07@gmail.com>
2024-09-18 13:18:04 +0545

    Update prac.py

--- prac.py
+++ prac.py
@@ -26,3 +26,32 @@ factorial = 1
 for i in range(1, n + 1):
     factorial *= i
 print(f"The factorial of {n} is {factorial}.")
+
+"""
+import numpy as np
+array_1d = np.array([10, 20, 30, 40])
+print("1D Array:", array_1d)
+
+import numpy as np
+array_2d = np.array([[10, 20, 30], [40, 50, 60]])
+print("2D array with 2 rows and 3 columns: \n", array_2d)
+
+
+import numpy as np  # Import the NumPy library
+
+# Create a 1D array with numbers from 1 to 100
+array_1d = np.arange(1, 101)
+
+# Calculate the sum of the array
+#array_sum = np.sum(array_1d)
+
+# Calculate the mean of the array
+#array_mean = np.mean(array_1d)
+
+# Print the results
+print("1D Array:", array_1d)
+#print("Sum of the array:", array_sum)
+#print("Mean of the array:", array_mean)
+"""
+
+print("hello all check project")
