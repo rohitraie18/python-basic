28997f9c6b052c25d621cde290cfeba35bd0792b
KHAL1NG HORAA <rohitraee07@gmail.com>
2024-09-21 19:29:05 +0545

    Update lecture_4.py

--- lecture_4.py
+++ lecture_4.py
@@ -1,6 +1,6 @@
 #Dictionary and set
 
-"""
+
 dict = {
     "name" : "Ram",
     "class" : 12,
@@ -25,5 +25,33 @@ student = {
 #print(student)
 print(student["subject"])
 print(student["subject"]["math"])
-"""
+
+student = {
+    "name" : "Roman",
+    "subject" : {
+        "phy" : 90,
+        "chem" : 95,
+        "math" : 97
+    }
+}
+
+print(student.keys())
+print(len(student))
+print(list(student.keys()))
+print(len(list(student.keys())))
+
+print(student.values())
+
+print(student.items)
+
+print(student["name"])  #print(student.get("name"))
+
+
+student.update({"city" : "KTM"}) 
+print(student)
+
+
+new = {"city" : "PKR"} #  student.update({"city" : "KTM"}) 
+ student.update(new)    #print(student)
+print(student)                       
 
