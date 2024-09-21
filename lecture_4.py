01923e24e8599cd7ae586eba6620aea0146b1b7a
KHAL1NG HORAA <rohitraee07@gmail.com>
2024-09-21 19:07:27 +0545

    Add files via upload

---
+++ lecture_4.py
@@ -0,0 +1,29 @@
+#Dictionary and set
+
+"""
+dict = {
+    "name" : "Ram",
+    "class" : 12,
+    "sub" : ["python", "java", "css"],
+    "marks" : [90, 88, 94]
+}
+print(dict)
+print(dict["name"])
+print(dict["marks"])
+print(dict["class"])
+print(dict["sub"])
+
+
+student = {
+    "name" : "Roman",
+    "subject" : {
+        "phy" : 90,
+        "chem" : 95,
+        "math" : 97
+    }
+}
+#print(student)
+print(student["subject"])
+print(student["subject"]["math"])
+"""
+
